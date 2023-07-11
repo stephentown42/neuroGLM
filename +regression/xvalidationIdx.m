d9e48f86151cea7614c866fa53763edaea54cd45
stephentown42 <s.town@ucl.ac.uk>
2023-07-11 13:09:02 +0100

    Minor docs and formatting changes

--- +regression/xvalidationIdx.m
+++ +regression/xvalidationIdx.m
@@ -10,7 +10,7 @@ function xidxs = xvalidationIdx(nTotalSamples, kxValidation, isRandomized, isPar
 % Output
 %   xidxs: {kxValidation x 2} training, test indices
 %
-% Caution: test set is not guarantteed 
+% Caution: test set is not guaranteed 
 %          to be a partition, UNLESS mod(nTotalSamples, kxValidation) == 0
 %          In such case, the test set is a partition of nTotalSamples.
 %	   Use isPartition to enforce partitioning.
@@ -18,19 +18,14 @@ function xidxs = xvalidationIdx(nTotalSamples, kxValidation, isRandomized, isPar
 % $Id$
 % Copyright 2011 Memming. All rights reserved.
 
-if nargin < 3
-    isRandomized = true;
-end
-
-if nargin < 4
-    isPartition = false;
-end
+if nargin < 3, isRandomized = true; end
+if nargin < 4, isPartition = false; end
 
 if isPartition
     nTotalSamples = nTotalSamples - rem(nTotalSamples, kxValidation);
 end
 
-if rem(nTotalSamples,1) ~= 0 | rem(kxValidation,1) ~= 0
+if rem(nTotalSamples,1) ~= 0 || rem(kxValidation,1) ~= 0
     error('xvalidationIdx', 'arguments should be both integers');
 end
 
@@ -52,17 +47,21 @@ if isRandomized
     ridx = ridx(sidx);
 end
 
-if kxValidation == 1
-    % No cross-validation. All samples are training, and all samples are test.
+% No cross-validation. All samples are training, and all samples are test.
+if kxValidation == 1    
     xidxs{1,1} = ridx;
     xidxs{1,2} = ridx;
     return
 end
 
+% K-fold train/test split
 startIdx = ceil(linspace(1, nTotalSamples, kxValidation+1));
-for k = 1:kxValidation
+
+for k = 1 : kxValidation
+    
     testSet = startIdx(k) + (1:m) - 1;
     trainSet = setdiff(1:nTotalSamples, testSet);
+    
     xidxs{k,1} = ridx(trainSet);
     xidxs{k,2} = ridx(testSet);
 end
